4e10e8e5b278ac0c227a9fcf979e386176eabdbe
Thomas Down <thomas.a.down@googlemail.com>
2013-06-27 10:41:46 +0100

    Adding static modifier to constants. Required by language changes in Beta 1 as per http://news.dartlang.org/2013/06/notes-from-june-4-dart-language-design.html

--- lib/src/inflater.dart
+++ lib/src/inflater.dart
@@ -10,10 +10,10 @@ part of zlib;
 
 class Inflater {
   // Constant parameters.
-  const WSIZE = 32768; // Sliding Window size.
-  const STORED_BLOCK = 0;
-  const STATIC_TREES = 1;
-  const DYN_TREES = 2;
+  static const WSIZE = 32768; // Sliding Window size.
+  static const STORED_BLOCK = 0;
+  static const STATIC_TREES = 1;
+  static const DYN_TREES = 2;
 
   // Base options.
   int literalBits = 9; // Bits in base literal/length lookup table.
@@ -48,35 +48,35 @@ class Inflater {
   int currentPosition = 0;
 
   // Constant tables.
-  const MASK_BITS = const [
+  static const MASK_BITS = const [
     0x0000,
     0x0001, 0x0003, 0x0007, 0x000f, 0x001f, 0x003f, 0x007f, 0x00ff,
     0x01ff, 0x03ff, 0x07ff, 0x0fff, 0x1fff, 0x3fff, 0x7fff, 0xffff];
 
   // Tables for deflate from PKZIP's appnote.txt.
-  const cplens = const [ // Copy lengths for literal codes 257..285.
+  static const cplens = const [ // Copy lengths for literal codes 257..285.
     3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31,
     35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258, 0, 0];
 
   // Note: see note #13 above about the 258 in this list.
-  const cplext = const [ // Extra bits for literal codes 257..285.
+  static const cplext = const [ // Extra bits for literal codes 257..285.
     0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2,
     3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0, 99, 99]; // 99 == invalid.
 
   // Copy offsets for distance codes 0..29.
-  const cpdist = const [
+  static const cpdist = const [
     1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193,
     257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145,
     8193, 12289, 16385, 24577];
 
   // Extra bits for distance codes.
-  const cpdext = const [
+  static const cpdext = const [
     0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,
     7, 7, 8, 8, 9, 9, 10, 10, 11, 11,
     12, 12, 13, 13];
 
   // Order of the bit length code lengths.
-  const border = const [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15];
+  static const border = const [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15];
 
   /**
    * Inflates the given data.
